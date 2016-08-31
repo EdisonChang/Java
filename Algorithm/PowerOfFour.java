11feb92dc3d63fab0fd00ba405ea75069e8fd74f
EdisonChang <edisonchang7@gmail.com>
2016-08-31 15:14:06 +0800

    Update and rename PowerOfFour,java to PowerOfFour.java

--- Algorithm/PowerOfFour.java
+++ Algorithm/PowerOfFour.java
@@ -16,28 +16,28 @@ public class PowerOfFour {
 	public static boolean isPowerOfFour(int num) {
 		//High performance approach use bit operation.//
 		if(num < 1) return false;
-	    int root = (int) Math.sqrt(num);
-	    System.out.println(num + " : " + Math.sqrt(num) + " " + (num-1) + " " + (num & (num-1)));
-	    return (num & (num-1)) == 0 && num - root * root == 0;
-
-	    //Returns the number of zero bits following the lowest-order ("rightmost") one-bit in the two's complement binary representation of the specified int value.//
+		int root = (int) Math.sqrt(num);
+		System.out.println(num + " : " + Math.sqrt(num) + " " + (num-1) + " " + (num & (num-1)));
+		return (num & (num-1)) == 0 && num - root * root == 0;
+		
+		//Returns the number of zero bits following the lowest-order ("rightmost") one-bit in the two's complement binary representation of the specified int value.//
 		//return num > 0 && (num & (num-1)) == 0 && Integer.numberOfTrailingZeros(num) % 2 == 0;
-	    
-	    //Change to binary string and use regular expression in one line.// 
-	    //System.out.println(num + " : " + Integer.toBinaryString(num) + " " + Integer.toBinaryString(num).matches("1([0]{2})*"));
+		
+		//Change to binary string and use regular expression in one line.// 
+		//System.out.println(num + " : " + Integer.toBinaryString(num) + " " + Integer.toBinaryString(num).matches("1([0]{2})*"));
 		//return Integer.toBinaryString(num).matches("1([0]{2})*");
-	    
-	    //Use regular expression.
-	    //if (num == 1) return true;
-        //int remain = num % 4;
+		
+		//Use regular expression.
+		//if (num == 1) return true;
+		//int remain = num % 4;
 		//if (remain == 0) {
 		//	Pattern pattern = Pattern.compile("[1]([0]{2})+");
 		//	Matcher isCorrect = pattern.matcher(Integer.toBinaryString(num));
 		//	System.out.println(num + " : " + Integer.toBinaryString(num) + " " + isCorrect.matches());
 		//	return isCorrect.matches();
 		//}
-        //return false;
-    }
+		//return false;
+	}
 	
 	public static void main(String[] args) {
 		isPowerOfFour(1);
